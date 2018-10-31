3cfc2f54c7c19a1cd6cc2ae0e47fab3d1ce80f0e
Penguino138 <Natelovell99@gmail.com>
2018-10-31 15:16:20 -0600

    Base Code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ExtendScript-osCheck
+Simply run the function osCheck(), whether from the same .jsx, or externally, and it will return either "PC" or "MAC"
---
+++ osCheck.jsx
@@ -0,0 +1,10 @@
+﻿function osCheck() {
+        var os = $.os;
+        var match = os.indexOf("Windows");
+        if(match != (-1)) {
+                var userOS = "PC";
+            } else {
+                 var userOS = "MAC";
+                }
+            return userOS;
+    }
